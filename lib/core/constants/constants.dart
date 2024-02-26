bfdfdfb4815b0e79005a09afd4f2d0b99fe4a951
Pavlo Holovachov <adamdoom23@gmail.com>
2024-02-26 22:23:06 +0200

    Added the core components

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+const String weatherAPIBaseURL = 'https://api.weatherapi.com/v1/forecast.json';
+const String weatherAPIKey = 'a8bc490c946d469a829140007231311';
+const String countryQuery = 'London';
+const String aqiQuery = 'no';
+const String alertsQuery = 'no';
