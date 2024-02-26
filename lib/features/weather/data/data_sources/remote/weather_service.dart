f5dbf7d4d09da71600193d4f45cc7daace9ba5a4
Pavlo Holovachov <adamdoom23@gmail.com>
2024-02-26 22:51:53 +0200

    Added the weather service

---
+++ lib/features/weather/data/data_sources/remote/weather_service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import 'package:weather_clean/core/constants/constants.dart';
+import 'package:weather_clean/features/weather/data/models/weather.dart';
+
+part 'weather_service.g.dart';
+
+@RestApi(baseUrl: weatherAPIBaseURL)
+abstract class WeatherApiService {
+  factory WeatherApiService(Dio dio) = _WeatherApiService;
+
+  @GET('/forecast.json')
+  Future<HttpResponse<WeatherModel>> getWeather({
+    @Query('key') String? apiKey,
+    @Query('q') String? place,
+    @Query('days') int? forecastDays,
+    @Query('aqi') int? airQuality,
+    @Query('alerts') int? alerts,
+  });
+}
